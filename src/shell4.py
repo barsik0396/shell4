96754c477578f1dc50114ad377b0fea19c97f20a
barsik0396 <Barsik0396@outlook.com>
2026-07-22 11:34:35 +0300

    add more commands and update version

--- src/shell4.py
+++ src/shell4.py
@@ -1,4 +1,4 @@
-print('welcome to \x1b[32mshell4\x1b[0m version 0.1!')
+print('welcome to \x1b[32mshell4\x1b[0m version 0.1.1!')
 import os
 while True:
     inp = input("shell4# \x1b[32m")
@@ -10,5 +10,16 @@ while True:
         pass
     elif inp.startswith('bin '):
         os.system(inp.removeprefix('bin '))
+    elif inp == "platform":
+        import platform
+        print(platform.platform())
+    elif inp == "help":
+        print("  help       show this help")
+        print("  exit       quit shell4")
+        print("  version    show shell4 version")
+        print("  bin <cmd>  run file from PATH")
+        print("  platform   show platform")
+    elif inp == 'version':
+        print("shell4 version v0.1.1")
     else:
         print('\x1b[31mUnknown command!\x1b[0m')
