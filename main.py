e9703a63ef07e539c4cd20ac4409a6e96dcb1cda
barsik0396 <Barsik0396@outlook.com>
2026-07-30 16:36:49 +0300

    add entry point

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys, traceback, os
+def exception(_, value, exc_traceback):
+    print("shell4 crashed. this isn't your problem; you have nothing to do with it.")
+    print("debugging information:")
+    tb = traceback.extract_tb(exc_traceback)
+    print(" === <RUNNER> ===")
+    for e in tb:
+        print(f" === {os.path.basename(e.filename).upper()} ===")
+        print(f"Line: {e.lineno}")
+        print(f"Function: {e.name}")
+        print(f"Code: {e.line}")
+    print(f"Error: {type(value).__name__}: {value}")
+    sys.exit(1)
+sys.excepthook = exception
+from src import shell4
+shell4.main()
