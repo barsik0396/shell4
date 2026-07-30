f3a1d55c48b7ff5b1ba6a06be08d3f47cda97e8b
barsik0396 <Barsik0396@outlook.com>
2026-07-30 16:42:10 +0300

    rewrite shell4 and add `crash` command

--- src/shell4.py
+++ src/shell4.py
@@ -1,25 +1,33 @@
-print('welcome to \x1b[32mshell4\x1b[0m version 0.1.1!')
 import os
-while True:
-    inp = input("shell4# \x1b[32m")
-    print('\x1b[0m', end='')
-    if inp == "exit":
-        import sys
-        sys.exit()
-    elif inp == "":
-        pass
-    elif inp.startswith('bin '):
-        os.system(inp.removeprefix('bin '))
-    elif inp == "platform":
-        import platform
-        print(platform.platform())
-    elif inp == "help":
-        print("  help       show this help")
-        print("  exit       quit shell4")
-        print("  version    show shell4 version")
-        print("  bin <cmd>  run file from PATH")
-        print("  platform   show platform")
-    elif inp == 'version':
-        print("shell4 version v0.1.1")
-    else:
-        print('\x1b[31mUnknown command!\x1b[0m')
+import sys
+import platform
+from .errors import CatsError
+
+def main():
+    print("welcome to \x1b[32mshell4\x1b[0m version v0.2.0p1!")
+    while True:
+        inp = input("shell4# \x1b[32m")
+        print("\r\x1b[0m\r", end="", flush=True)
+        if inp == "exit":
+            print("Bye!")
+            sys.exit()
+        elif inp == "":
+            pass
+        elif inp.startswith("bin "):
+            os.system(inp.removeprefix("bin "))
+        elif inp == "platform":
+            print(platform.platform())
+        elif inp == "version":
+            print("shell4 version v0.2.0p1")
+        elif inp == "help":
+            print("Available commands:")
+            print("  help        show this help")
+            print("  exit        quit")
+            print("  version     show version")
+            print("  bin <cmd>   run non-shell4 command")
+            print("  platform    show platform")
+            print("  crash       crash shell4")
+        elif inp == "crash":
+            raise CatsError.CatsError("shell4 is too good.")
+        else:
+            print("\x1b[31mUnknown command!\x1b[0m")
