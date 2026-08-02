53f6d4040888fc1de1a604c8ba13d0d922b014c9
barsik0396 <Barsik0396@outlook.com>
2026-08-02 23:20:47 +0300

    update version to v0.2.0p2

--- src/shell4.py
+++ src/shell4.py
@@ -15,7 +15,7 @@ except Exception:                     # noqa: BLE001
         f.write("meow")
 
 def main():
-    print("welcome to \x1b[32mshell4\x1b[0m version v0.2.0p1!")
+    print("welcome to \x1b[32mshell4\x1b[0m version v0.2.0p2!")
     while True:
         inp = input("shell4# \x1b[32m")
         print("\r\x1b[0m\r", end="", flush=True)
@@ -29,7 +29,7 @@ def main():
         elif inp == "platform":
             print(platform.platform())
         elif inp == "version":
-            print("shell4 version v0.2.0p1")
+            print("shell4 version v0.2.0p2")
         elif inp == "help":
             print("Available commands:")
             print("  help        show this help")
