08b894ff33fc0756e7786a63e0ab23ea1fc85fb9
barsik0396 <Barsik0396@outlook.com>
2026-08-08 13:14:29 +0300

    add lang loader

---
+++ src/loadlang.py
@@ -0,0 +1,9 @@
+import json, os
+
+with open(os.path.expanduser("~/.shell4/config/language"), "r") as f:
+    _lang = f.read()
+
+def load():
+    global lang
+    with open(os.path.expanduser(f"~/.shell4/languages/{_lang}.lang"), "r") as f:
+        lang = json.loads(f.read())
